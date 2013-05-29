dabe8731909c4399fb788b04c6191a14cc84e883
Eric <Ericrius1@gmail.com>
2013-05-29 01:28:43 -0700

    added shader to represent contributor

--- globe/globe.js
+++ globe/globe.js
@@ -76,6 +76,23 @@ DAT.Globe = function(container) {
           'gl_FragColor = vec4( 0.33, 0.11, 0.44, 1.0 ) * intensity;',
           '}'
       ].join('\n')
+    },
+    'contributor': {
+      uniforms: {},
+      vertexShader: [
+        'varrying vec3 vNormal;',
+        'void main() {',
+        'vNormal = normalize( normalMatrix * normal);',
+        'gl_position = projectorMatrix * modelViewMatrix * vec4(position, 1.0);',
+        '}'
+      ].join('\n'),
+      fragmentShader: [
+      'varying vec3 vNormal;',
+      'void main() {',
+      'float intensity = pow(0.8 - dot( vNormal, vec3 (0, 0, 1.0)), 12.0);',
+      'gl_FragColor = vec4(0.33, 0.11, 0.44, 1.0 ) * intensity;',
+      '}'
+      ].join('\n')
     }
   };
 
